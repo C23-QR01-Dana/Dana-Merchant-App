1a683bd5334808c483f82b6cfefde75cf107ebf5
Muhammad Asyraf <muhammad.asyraf@ui.ac.id>
2023-05-21 15:32:23 +0700

    [FEAT] Implement register with fireStore

--- app/src/main/java/com/dana/merchantapp/data/register/RegisterRepositoryImpl.kt
+++ app/src/main/java/com/dana/merchantapp/data/register/RegisterRepositoryImpl.kt
@@ -2,16 +2,39 @@ package com.dana.merchantapp.data.register
 
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class RegisterRepositoryImpl : RegisterRepository {
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun registerUser(name: String, address: String,email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val uid = user?.uid
 
+                    val merchantData = hashMapOf(
+                        "balance" to 0,
+                        "email" to email,
+                        "merchantAddress" to address,
+                        "merchantName" to name
+                    )
+
+                    if (uid != null) {
+                        firestore.collection("merchant").document(uid)
+                            .set(merchantData)
+                            .addOnSuccessListener {
+                                Log.v("register", "Data successfully saved to Firestore")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e("register", "Error saving data to Firestore", e)
+                            }
+                    } else {
+                        Log.e("register", "User UID is null")
+                    }
                 } else {
                     val errorMsg = task.exception?.message ?: "Registration failed"
                     Log.e("register", errorMsg)
